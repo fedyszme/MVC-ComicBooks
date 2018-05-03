a0040797f39ffb4460cc66e57d2fdd3c73d33349
fedyszme <36646062+fedyszme@users.noreply.github.com>
2018-05-03 10:25:15 -0400

    Adding files

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(x => x.ComicBookId == id);
+
+
+            return View(comic);
+        }
+    }
+}
